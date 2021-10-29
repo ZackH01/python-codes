7b7f592c17cd857abc9a7f07542bf37177501b38
ZackH <93035068+ZackH01@users.noreply.github.com>
2021-10-29 16:44:37 +0100

    Find next prime

--- prime_factors.py
+++ prime_factors.py
@@ -1,3 +1,18 @@
+def next_prime(n):
+    #Finds the next prime number after n
+    #Check numbers after n until one is found with factors 1 and itself
+    has_factor = True
+    
+    while has_factor:
+        n += 1
+        has_factor = False
+        for i in range(2, n):
+            if n % i == 0:
+                has_factor = True
+
+    return n
+
+
 def prime_factors(n):
     #Divide by first prime
     #Keep dividing until division isn't an integer
@@ -9,8 +24,8 @@ def prime_factors(n):
         n /= curr_prime
         factors.append(curr_prime)
 
-
     return factors
 
 
 print(prime_factors(8))
+print(next_prime(1))
