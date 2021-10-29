17aa4a84eb9bd6dddfeb3d81e467a369539efe1c
ZackH <93035068+ZackH01@users.noreply.github.com>
2021-10-29 17:05:31 +0100

    Finds prime factors and formats output

--- prime_factors.py
+++ prime_factors.py
@@ -17,15 +17,50 @@ def prime_factors(n):
     #Divide by first prime
     #Keep dividing until division isn't an integer
     #Repeat for next primes until original number is 1
+    #Returns list of factors of n (or n rounded down)
+    n = int(n//1)
     factors = []
     curr_prime = 2
 
-    while n % curr_prime == 0:
-        n /= curr_prime
-        factors.append(curr_prime)
+    while n > 1:
+        while n % curr_prime == 0:
+            n /= curr_prime
+            factors.append(curr_prime)
+        curr_prime = next_prime(curr_prime)
 
     return factors
 
 
-print(prime_factors(8))
-print(next_prime(1))
+def prime_factors_string(n):
+    #Formats output as string
+    factors = prime_factors(n)
+    unique_factors = []
+    for x in factors:
+        if x not in unique_factors:
+            unique_factors.append(x)
+
+    string = ""
+    exponent = 1
+    for x in factors:
+        if x in unique_factors:
+            if exponent != 1:
+                string += ("^" + str(exponent))
+            if x != factors[0]:
+                string += (" * " + str(x))
+            else:
+                string += str(x)
+            exponent = 1
+            unique_factors.remove(x)
+        else:
+            exponent += 1
+        print(string)
+
+    if exponent != 1:
+        string += ("^" + str(exponent))
+
+    return string
+
+
+print(prime_factors(10080))
+print(prime_factors_string(10080))
+
