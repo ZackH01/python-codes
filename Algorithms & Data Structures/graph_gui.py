058cc68c0aa8a6e379efc12f22ce0582c296dbec
ZackH <93035068+ZackH01@users.noreply.github.com>
2021-12-12 15:10:01 +0000

    Refactor code into classes, refine drawing of the graph

--- Algorithms & Data Structures/graph_gui.py
+++ Algorithms & Data Structures/graph_gui.py
@@ -2,56 +2,108 @@ from tkinter import Tk, Canvas
 from graph import Graph
 
 
-def draw_nodes(graph):
-    node_list = graph.nodes
-    size = 50
-    seperation = 200
-    offset = 100
+class MainWindow:
+    def __init__(self, width, height, title):
+        self.window = Tk()
+        self.window.geometry(str(width)+"x"+str(height))
+        self.window.title(str(title))
+
+    def display(self):
+        self.window.mainloop()
+
+
+class GraphCanvas:
+    def __init__(self, window, graph):
+        self.canvas = Canvas(window, width=500, height=500)
+        self.graph = graph
+
+
+    #Completely revamp drawing
+    """
+    @staticmethod
+    def count_node_edges(graph):
+        #Returns a list of the nodes with their corresponding edges
+        edges = graph.edges
+        edge_count = [] #List of pairs [node, edge count]
+
+        for i, node in enumerate(graph.nodes):
+            edge_count.append([node, 0]) #[node_id, edge_count]
+            for edge in edges:
+                if edge[0] == node:
+                    edge_count[i][1] += 1
+        
+        return edge_count
+    """
     
-    #Create a circle to represent each node
-    drawn_nodes = {}
-    for i, node in enumerate(node_list):
-        x = seperation*i +offset
-        y = 100
-        drawn_nodes[node] = canvas.create_oval(x, y, x+size, y+size, fill="blue")
+    @staticmethod
+    def find_node_pos(graph):
+        #Returns a list of relative positions of each node's centre
+        nodes = graph.nodes
+        #edges = graph.edges
+        node_pos = [] #List of [node_id, x_pos, y_pos]
 
-    return drawn_nodes #Dictionary mapping node id's to canvas objects
+        #Put nodes in a list
+        for i, node in enumerate(nodes):
+            node_pos.append([node, i, 0])
 
-
-def draw_edges(graph):
-    edge_list = graph.edges
-
-    #Draw a line from the centre of a node to another to represent and edge
-    drawn_edges = []
-    for i, edge in enumerate(edge_list):
-        node_pos = canvas.coords(drawn_nodes.get(edge[0]))
-        start_node_centre = [node_pos[0]+(node_pos[2]-node_pos[0])/2, node_pos[1]+(node_pos[3]-node_pos[1])/2]
-        node_pos = canvas.coords(drawn_nodes.get(edge[1]))
-        end_node_centre = [node_pos[0]+(node_pos[2]-node_pos[0])/2, node_pos[1]+(node_pos[3]-node_pos[1])/2]
-
-        drawn_edges.append(canvas.create_line(start_node_centre[0], start_node_centre[1], end_node_centre[0], end_node_centre[1]))
-
-    return draw_edges
+        return node_pos
 
 
-if __name__ == "__main__":
+    def draw_graph(self):
+        node_positions = self.find_node_pos(self.graph)
+        edges = self.graph.edges
+
+        #Variables that affect the appearence of the image
+        size = 25 #Radius of each node
+        x_offset = 100
+        y_offset = 150
+        spacing = 150
+        colour = "blue"
+        thickness = 5
+
+        #Draw edges
+        for edge in edges:
+            #Find start and end points for the edge
+            for node in node_positions:
+                if edge[0] == node[0]:
+                    start_node = node
+                if edge[1] == node[0]:
+                    end_node = node
+
+            x1 = start_node[1]*spacing+x_offset
+            y1 = start_node[2]*spacing+y_offset
+            x2 = end_node[1]*spacing+x_offset
+            y2 = end_node[2]*spacing+y_offset
+            self.canvas.create_line(x1, y1, x2, y2, width=thickness)
+
+        #Draw nodes
+        for node in node_positions:
+            x1 = node[1]*spacing-size+x_offset
+            y1 = node[2]*spacing-size+y_offset
+            x2 = node[1]*spacing+size+x_offset
+            y2 = node[2]*spacing+size+y_offset
+            self.canvas.create_oval(x1, y1, x2, y2, fill=colour)
+
+        self.canvas.pack()
+
+
+def main():
     #Create window
-    window = Tk()
-    window.title("Graph")
-    window.geometry("500x500")
-    canvas = Canvas(window, height=500, width=500)
-    canvas.pack()
+    main_window = MainWindow(500, 500, "Graph")
 
     #Create test graph
     my_graph = Graph()
     my_graph.add_node()
     my_graph.add_node()
-    my_graph.add_edge(0, 1)
+    my_graph.add_node()
+    my_graph.add_edge(1, 0)
+    my_graph.add_edge(1, 2)
 
     #Draw graph
-    drawn_nodes = draw_nodes(my_graph)
-    draw_edges(my_graph)
-    
-    window.mainloop()
+    graph_image = GraphCanvas(main_window.window, my_graph)
+    graph_image.draw_graph()
+
+    main_window.display()
 
 
+if __name__ == "__main__": main()
